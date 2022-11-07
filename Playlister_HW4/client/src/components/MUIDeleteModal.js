6e25fcb4c669df58418baf3d8d7c56546d172a48
aakanojia <aaryamann.kanojia@stonybrook.edu>
2022-11-07 17:14:39 -0500

    Fixed deleting a list

--- Playlister_HW4/client/src/components/MUIDeleteModal.js
+++ Playlister_HW4/client/src/components/MUIDeleteModal.js
@@ -3,6 +3,7 @@ import GlobalStoreContext from '../store';
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
+import { Button, Typography } from '@mui/material';
 
 const style = {
     position: 'absolute',
@@ -26,7 +27,7 @@ export default function MUIDeleteModal() {
         store.deleteMarkedList();
     }
     function handleCloseModal(event) {
-        store.unmarkListForDeletion();
+        store.hideModals();
     }
 
     return (
@@ -34,23 +35,21 @@ export default function MUIDeleteModal() {
             open={store.listMarkedForDeletion !== null}
         >
             <Box sx={style}>
-                <div className="modal-dialog">
-                <header className="dialog-header">
-                    Delete the {name} Top 5 List?
-                </header>
-                <div id="confirm-cancel-container">
-                    <button
-                        id="dialog-yes-button"
-                        className="modal-button"
+                <Box>
+                    <Typography variant='h4'>
+                        Delete the <span>{name}</span> Play List?
+                    </Typography>
+                </Box>
+                <Box>
+                    <Button
+                        sx={{fontSize: '18px'}}
                         onClick={handleDeleteList}
-                    >Confirm</button>
-                    <button
-                        id="dialog-no-button"
-                        className="modal-button"
+                    >Confirm</Button>
+                    <Button
+                        sx={{float: 'right', fontSize: '18px'}}
                         onClick={handleCloseModal}
-                    >Cancel</button>
-                </div>
-            </div>
+                    >Cancel</Button>
+                </Box>
             </Box>
         </Modal>
     );
